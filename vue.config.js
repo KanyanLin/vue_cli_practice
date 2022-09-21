4b53f85e46b06b783dcf743bae3240846b5701fe
SWLin <yy3282001@gmail.com>
2022-09-21 18:09:55 +0800

    git pages部屬路徑

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/dist/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue_cli_practice/'
+    : '/dist/'
 })
