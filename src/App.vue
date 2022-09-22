3b835d02448ed97c970cdc13eb81d5d0013faedc
SWLin <yy3282001@gmail.com>
2022-09-22 17:09:32 +0800

    加入 Bootstrap的Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link
-      :to="{
-        name: '新增頁面'
-      }"
-      >新增頁面</router-link
-    >
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">Home</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link"
+              :to="{
+                name: '新增頁面'
+              }"
+              >新增頁面</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
-  <router-view />
-  {{ name }}
+  <div class="container">
+    <router-view />
+    {{ name }}
+  </div>
 </template>
 
 <script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home container">
+  <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
 
